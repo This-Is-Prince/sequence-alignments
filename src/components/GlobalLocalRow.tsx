022cd8b527d8c4147272428b07b520fe6dccecde
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-18 23:43:39 +0530

    first commit

---
+++ src/components/GlobalLocalRow.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+
+const GlobalLocalRow: FC<{ str: string }> = ({ str }) => {
+  return (
+    <tr className="flex flex-1">
+      {str.split("").map((v, index) => {
+        console.log(v.length);
+        return (
+          <td
+            className="flex-1 text-center text-xs border-[1px] border-black md:text-base md:text-bold"
+            key={index}
+          >
+            {v}
+          </td>
+        );
+      })}
+    </tr>
+  );
+};
+
+export default GlobalLocalRow;
